a9b0f7159d99914ffe59274b33e42317a0afae33
Eric "Doc" Ritezel <eng@emimusic.com>
2011-01-06 11:05:43 -0800

    add suffixes to the fake name

--- javascripts/fake_name.js
+++ javascripts/fake_name.js
@@ -9,20 +9,47 @@ var fakeName = (function(){
   var families = [
     "Smith", "Jones", "Braun", "Hammer", "Honda",
     "Michaels", "Robson", "Krupp", "Stevens", "Laporte",
-    "Simonoff", "Rainbow", "Karimov"
+    "Simon", "Rain", "Karim"
   ];
+  
+  var suffixes = [
+    "stein", "son", "bow", "off", "ich", "ist"
+  ];
+  
+  var stops = ['a', 'e', 'i', 'o', 'u', 'y', 's'];
+  
+  function isSuffixable(letter) {
+    var can = false;
+    
+    for (var s = 0; !can && s < stops.length; s++) {
+      can = (letter.localeCompare(stops[s]) == 0);
+    }
+    
+    return can;
+  }
+  
+  function suffixFamily(family) {
+    if (Math.round(Math.random()) == 0 && !isSuffixable(family[family.length - 1])) {
+      family = family + suffixes[Math.floor(Math.random() * suffixes.length)];
+    }
+    
+    return family;
+  }
   
   return function(parameter) {
     var surname, family;
     
     if (parameter) {
-      surname = parameter % (surnames.length);
-      family = parameter % (families.length);
+      surname = parameter % surnames.length;
+      family = parameter % families.length;
     } else {
-      surname = Math.floor(Math.random() * (surnames.length));
-      family = Math.floor(Math.random() * (families.length));
+      surname = Math.floor(Math.random() * surnames.length);
+      family = Math.floor(Math.random() * families.length);
     }
     
-    return { first_name: surnames[surname], last_name: families[family] };
+    return {
+      first_name: surnames[surname],
+      last_name: suffixFamily(families[family])
+    };
   }
 })();
